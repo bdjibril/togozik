6fed15a985d7135cd2a99f804ec63e4bb361b1f9
Bah Djibril <dbdjbah@gmail.com>
2013-08-23 23:36:05 -0400

    Addd the menu controller

---
+++ app/scripts/controllers/menu.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('togozikApp')
+  .controller('MenuCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
